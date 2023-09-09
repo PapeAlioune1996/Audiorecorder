b30bba83801d1c112c22860edd9f20beab139500
unknown <PapeAlioune1996>
2023-09-09 21:09:59 +0200

    first commit

---
+++ src/com/audio/AudioRecorder.java
@@ -0,0 +1,55 @@
+package com.audio;
+
+import java.io.File;
+
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.TargetDataLine;
+
+
+public class AudioRecorder {
+	  
+	  private AudioFormat audioFormat;
+	    private TargetDataLine targetDataLine;
+
+	    public void startRecording(String outputFileName) {
+	        try {
+	            audioFormat = new AudioFormat(16000, 16, 2, true, true);
+	            DataLine.Info dataLineInfo = new DataLine.Info(TargetDataLine.class, audioFormat);
+	            targetDataLine = (TargetDataLine) AudioSystem.getLine(dataLineInfo);
+	            targetDataLine.open(audioFormat);
+	            targetDataLine.start();
+
+	            AudioInputStream audioInputStream = new AudioInputStream(targetDataLine);
+
+	            File audioFile = new File(outputFileName);
+	            AudioSystem.write(audioInputStream, AudioFileFormat.Type.WAVE, audioFile);
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	    }
+
+	    public void stopRecording() {
+	        if (targetDataLine != null) {
+	            targetDataLine.stop();
+	            targetDataLine.close();
+	        }
+	    }
+
+	    public static void main(String[] args) {
+	        AudioRecorder recorder = new AudioRecorder();
+	        recorder.startRecording("output.wav");
+	        System.out.println("Recording...");
+	        try {
+	            Thread.sleep(5000); // Enregistrement pendant 5 secondes
+	        } catch (InterruptedException e) {
+	            e.printStackTrace();
+	        }
+	        recorder.stopRecording();
+	        System.out.println("Recording stopped.");
+	    }
+	}
+
